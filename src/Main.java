704620e6104f008bb2dfe7bbfbcb91eec9bbe6fb
zs <zs@bjpowernode.com>
2023-01-01 18:39:00 +0800

    000

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("sbsbsbsb");
+        int a[]={18,1,2,5,6,3,8,9,5,22};
+        for(int i=0;i<a.length-1;i++){
+            for(int j=0;j<a.length-i-1;j++){
+                if(a[j]>a[j+1]){
+                  int temp=a[j];
+                  a[j]=a[j+1];
+                  a[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.print(" " +a[i]);
+        }
     }
 }
